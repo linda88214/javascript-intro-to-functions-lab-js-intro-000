2261b3f71b73bd8f1f46a502bb33c822b618601a
Yerim Hu <yerimhu88@gmail.com>
2017-10-31 11:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,4 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase()) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase()) {
-    return "YES INDEED!"
-  }
 }
